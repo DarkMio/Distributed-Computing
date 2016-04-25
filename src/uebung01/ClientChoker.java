fd2a10bdf30c2931f86fe87ec0b703fb3bf2ff3b
s59330 <s59330@beuth-hochschule.de>
2016-04-25 16:28:48 +0200

    finishing, choker works (or rather, the server doesn't choke on it.

--- src/uebung01/ClientChoker.java
+++ src/uebung01/ClientChoker.java
@@ -4,26 +4,11 @@ import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
 
-/**
- * Created by Mio on 25.04.2016.
- */
 public class ClientChoker {
 
     public static void main(String[] args) {
-        String hostName;
-        int portNumber;
-        if(args.length >=2){
-            hostName = args[0];
-            portNumber = Integer.parseInt(args[1]);
-        } else if (args.length == 1){
-            hostName = args[0];
-            portNumber = 5678;
-        } else {
-            System.err.println("No IP in args");
-            System.exit(-1);
-            hostName = "";
-            portNumber = 0;
-        }
+        String hostName = "127.0.0.1";
+        int portNumber = 5678;
         try {
             Socket kkSocket = new Socket(hostName, portNumber);
             PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
@@ -31,23 +16,11 @@ public class ClientChoker {
             DataInputStream dis = new DataInputStream(kkSocket.getInputStream());
             DataOutputStream dos = new DataOutputStream(kkSocket.getOutputStream());
             while (true) {
-
-                String fromUser;
-                BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-                System.out.print("Input> ");
-                fromUser = input.readLine();
-                if(fromUser != null) {
-                    out.println("Choke the server to its sudden death.");
-
-                    for(int i = 0; i < 150; i++) {
-                        System.out.println(in.read());
-                    }
-                } else {
-                    break;
+                out.println("Choke the server to its sudden death.");
+                for(int i = 0; i < 150; i++) {
+                    System.out.println(dis.readInt());
                 }
-
             }
-            kkSocket.close();
         } catch (SocketException e) {
             System.err.println("Server forcibly closed the connection");
         } catch (IOException e) {
