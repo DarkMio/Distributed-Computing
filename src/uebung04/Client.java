18b5269f26bee1562d6b87f088965f86bb28134f
Fabian Wendland <s56503@beuth-hochschule.de>
2016-05-30 13:52:33 +0200

    Server-Cllient String exchange

--- src/uebung04/Client.java
+++ src/uebung04/Client.java
@@ -1,7 +1,55 @@
 package uebung04;
 
+import uebung01.Client.FibonacciProtocol;
+import uebung01.Client.ProtocolException;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 /**
  * Created by Mio on 30.05.2016.
  */
 public class Client {
+
+    public static void main(String[] args) {
+        String hostName;
+        int portNumber;
+        if (args.length >= 2) {
+            hostName = args[0];
+            portNumber = Integer.parseInt(args[1]);
+        } else if (args.length == 1) {
+            hostName = args[0];
+            portNumber = 5678;
+        } else {
+            System.err.println("No IP in args");
+            System.exit(-1);
+            hostName = "";  //Makes inspection & compiler warnings happy
+            portNumber = 0; // about uninitialized variables down below
+        }
+        try {
+            Socket socket = new Socket(hostName, portNumber);
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                String fromUser;
+                System.out.print("Input> ");
+                fromUser = inFromUser.readLine();
+                if (fromUser != null) {
+                    outToServer.writeBytes(fromUser + "\n");
+                    String test2 = inFromServer.readLine();
+                    System.out.println(test2);
+                } else {
+                    break;
+                }
+                socket.close();
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/uebung04/Server.java
+++ src/uebung04/Server.java
@@ -1,7 +1,36 @@
 package uebung04;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
 /**
  * Created by Mio on 30.05.2016.
  */
+
 public class Server {
+
+    public static final int PORT = 5678;
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            Socket clientSocket = serverSocket.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(clientSocket.getOutputStream());
+            // Initiate conversation with client
+            while (true) {
+                String read = in.readLine();
+                System.err.println(read);
+                if(read != null){
+
+                    String test = "String accepted";
+                    outToClient.writeBytes(test + "\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
